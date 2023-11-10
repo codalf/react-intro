13494c0b7fdc430de0acf3423ba9c1fbd9050772
codalf <89524228+codalf@users.noreply.github.com>
2023-11-10 10:19:48 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+import "./App.css";
+
+export function App(): ReactElement {
+  return (
+    <>
+      <h1>My page</h1>
+      <div>Hello world</div>
+    </>
+  );
+}
